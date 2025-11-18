125e942f8366f0b30cba62b2384575df593a6298
UEFI-code <zhuoshizhang@hotmail.com>
2025-11-18 17:46:14 +0900

    Improve Code

--- client/Mac/wiper.py
+++ client/Mac/wiper.py
@@ -9,32 +9,25 @@ magicWord = 'WipeAllDataNow'
 webHooks = ['http://127.0.0.1:5000/command'] # replace to your server address
 
 garbages = ['Shared', '.localized', '.DS_Store']
-users = os.listdir('/Users/')
-real_users = [u for u in users if u not in garbages]
+real_users = [u for u in os.listdir('/Users/') if u not in garbages]
 
-needWipe = False
-
-while True:
+def chk_need_wipe():
     for u in real_users:
         # Get the path of the user's icloud folder
         icloud_path = '/Users/' + u + '/Library/Mobile Documents/com~apple~CloudDocs/'
         wipeFlagPath = icloud_path + magicWord
-        if os.path.exists(wipeFlagPath):
-            needWipe = True
-            break
+        if os.path.exists(wipeFlagPath): return True
     for hook in webHooks:
-        #fetch the web hook
         try:
             content = requests.get(hook, headers={'Cache-Control': 'no-cache'}, timeout=3).content.decode('utf-8')
             #print('Received: ' + content)
-            if content == magicWord:
-                needWipe = True
-                break
+            if content == magicWord: return True
         except:
             print("Net Error")
-            pass
+    return False
 
-    if needWipe:
+while True:
+    if chk_need_wipe():
         print(time.ctime(), 'Wiping all data now...')
         os.system("rm -rf /Library/Keychains/* &")
         os.system("rm -rf /Network/Library/Keychains/* &")
